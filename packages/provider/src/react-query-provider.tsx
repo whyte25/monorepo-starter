acb22ec2516e86ccebeff2bfe213f57aedbef988
whyte25 <fasifeoluwa3@gmail.com>
2025-01-01 11:08:16 +0100

    chore: add comment to explain tanstack query default options and automatic invalidatation

--- packages/provider/src/react-query-provider.tsx
+++ packages/provider/src/react-query-provider.tsx
@@ -5,6 +5,24 @@ import {
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+/**
+ * Initializes a new instance of QueryClient with default options and mutation cache.
+ *
+ * The `QueryClient` is configured with the following default options:
+ * - `retry`: Disabled (set to `false`).
+ * - `refetchOnWindowFocus`: Enabled (set to `true`).
+ * - `refetchOnReconnect`: Always refetch on reconnect (set to `"always"`).
+ * - `staleTime`: Queries will never go stale (set to `Number.POSITIVE_INFINITY`).
+ *
+ * Additionally, the `mutationCache` is configured to automatically invalidate queries
+ * after mutations. This is achieved by using the `mutationKey` to efficiently invalidate
+ * related queries. Mutations without a key will still invalidate all queries.
+ * For example, using `mutationKey: ['issues']` will invalidate all queries related to 'issues'.
+ * Mutations without a key will still invalidate all queries.
+ *
+ * For more details on automatic query invalidation after mutations, see:
+ * https://tkdodo.eu/blog/automatic-query-invalidation-after-mutations#tie-it-to-the-mutationkey
+ */
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
